404c42feb7d6ddf6894ddd85084cafb02c686382
hugomorenoo <hugomoreno1614@gmail.com>
2023-12-01 12:52:39 +0100

    primer commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+# Ruta para mostrar los alimentos desde la base de datos
+@app.route('/')
+def mostrar_alimentos():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM alimentos')
+    alimentos = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', alimentos=alimentos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
